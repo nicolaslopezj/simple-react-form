59bf4b3afb45b20bc60d7904f632833a5b12e386
Nicolás López <nicolaslopezj@me.com>
2016-06-08 12:05:18 -0400

    remove example

--- example/imports/register-schema.js
+++
@@ -1,5 +0,0 @@
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-
-SimpleSchema.extendOptions({
-  srf: Match.Optional(Object),
-});
--- example/main.js
+++
@@ -1,8 +0,0 @@
-import 'simple-react-form-material-ui';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import './imports/register-schema';
-
-import './imports/components/home/routes';
-import './imports/components/posts/routes';
-
-injectTapEventPlugin();
