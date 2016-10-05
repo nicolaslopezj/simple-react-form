ae10df63e5fc6c3336d87b8e7debdc3bb6e56bba
Fernando Mumbach (Fermuch) <fermuch@cryptolab.net>
2016-10-05 10:06:10 -0300

    added tests to Array field

--- src/array.test.js
+++ src/array.test.js
@@ -14,6 +14,47 @@ it('should render correctly', () => {
   expect(component.find('ArrayComponent').length).toBe(1);
 });
 
-xit('addItem should add an item', () => {})
-xit('removeItem should remove the item', () => {})
-xit('should render an error if there is one', () => {})
+it('addItem should add an item', () => {
+  const component = mount(
+    <Form>
+      <Field fieldName="array" type={ArrayField}>
+        <div className="children" />
+      </Field>
+    </Form>
+  )
+
+  const beforePress = component.find('.children').length
+  expect(beforePress).toBe(0)
+  component.find('button').simulate('click')
+  const afterPress = component.find('.children').length
+
+  expect(afterPress).toBe(beforePress + 1)
+})
+
+it('removeItem should remove the item', () => {
+  const component = mount(
+    <Form>
+      <Field fieldName="array" type={ArrayField}>
+        <div className="children" />
+      </Field>
+    </Form>
+  )
+  // add the first component
+  component.find('button').simulate('click')
+  const beforePress = component.find('.children').length
+  expect(beforePress).toBe(1)
+  // press the remove button
+  component.find('ArrayContextItem').parent().find('button').simulate('click')
+  const afterPress = component.find('.children').length
+
+  expect(afterPress).toBe(beforePress - 1)
+})
+
+it('should render an error if there is one', () => {
+  const component = mount(
+    <Form>
+      <Field fieldName="array" type={ArrayField} errorMessage="I AM AN ERROR"/>
+    </Form>
+  )
+  expect(component.find('div').first().childAt(1).text()).toBe('I AM AN ERROR')
+})
