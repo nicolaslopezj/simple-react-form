95e17a668f766b826ffc265f9ae78f420c516e20
Nicolás López <nicolaslopezj@me.com>
2023-01-11 12:50:38 -0300

    expose context hooks

--- src/Contexts.tsx
+++ src/Contexts.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 const ValueContext = React.createContext({})
 const ParentFieldNameContext = React.createContext<string>(null)
 const ErrorMessagesContext = React.createContext({})
 const OnChangeContext = React.createContext((fieldName: string, fieldValue: any) => {})
+
+export function useFormValueContext() {
+  return useContext(ValueContext)
+}
+export function useFormParentFieldNameContext() {
+  return useContext(ParentFieldNameContext)
+}
+export function useErrorMessagesContext() {
+  return useContext(ErrorMessagesContext)
+}
+export function useFormOnChangeContext() {
+  return useContext(OnChangeContext)
+}
 
 export {ValueContext, ErrorMessagesContext, OnChangeContext, ParentFieldNameContext}
--- src/index.tsx
+++ src/index.tsx
@@ -4,24 +4,8 @@ import ArrayComponent from './Array'
 import ObjectComponent from './Object'
 import WithValue from './WithValue'
 import useValue from './useValue'
-import {
-  ValueContext,
-  ErrorMessagesContext,
-  OnChangeContext,
-  ParentFieldNameContext
-} from './Contexts'
 
-export {
-  Form,
-  Field,
-  ArrayComponent,
-  ObjectComponent,
-  WithValue,
-  useValue,
-  ValueContext,
-  ErrorMessagesContext,
-  OnChangeContext,
-  ParentFieldNameContext
-}
+export {Form, Field, ArrayComponent, ObjectComponent, WithValue, useValue}
 
+export * from './Contexts'
 export * from './types'
